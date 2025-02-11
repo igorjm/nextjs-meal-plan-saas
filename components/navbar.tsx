0cebb68963953ceaee332a3c0cfccbfd67c76615
Igor Melo <igorjmelo4@gmail.com>
2025-02-11 19:57:25 -0300

    feat: add logo + starting navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav>
+      {""}
+      <div>
+        <Link href="/">
+          <Image src="/logo.png" width={60} height={60} alt="Logo" />
+        </Link>
+      </div>
+      <div></div>
+    </nav>
+  );
 }
 
 export default Navbar;
